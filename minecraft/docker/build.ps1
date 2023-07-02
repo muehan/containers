b2ea516d15124e8e404fe2843d0b9ca133bf7785
André <tdc@gmx.ch>
2023-07-02 13:28:16 +0200

    add minecraft docker container

---
+++ minecraft/docker/build.ps1
@@ -0,0 +1,2 @@
+docker build -t minecraft_creative:v1.20.1 . --build-arg gamemode=creative
+docker build -t minecraft_survival:v1.20.1 . --build-arg gamemode=survival
---
+++ minecraft/docker/push.ps1
@@ -0,0 +1,4 @@
+docker tag minecraft_creative:v1.20.1 muehan/minecraft_creative:latest
+docker tag minecraft_survival:v1.20.1 muehan/minecraft_survival:latest
+docker image push muehan/minecraft_creative:latest
+docker image push muehan/minecraft_survival:latest
